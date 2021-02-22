d3064c128db76270debff85a93ad9330da3b85b6
Robin <hej@robin.se>
2021-02-22 00:02:14 -0600

    WIP

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,72 +1,143 @@
 <template>
   <div>
-    <div class="map-input">
+    <div class="sm:flex absolute top-0 right-0 left-0 z-10 m-2 sm:m-4">
       <vue-autosuggest
+          class="sm:mr-4 mb-2 sm:mb-0"
           v-model="query"
           :suggestions="filteredCountries"
           :get-suggestion-value="getSuggestionValue"
-          :input-props="{id:'autosuggest__input', placeholder: 'Enter a country'}"
+          :input-props="{id: 'autosuggest__input', class: 'input', placeholder: 'Enter a country'}"
           @selected="addCountry($event.item.cca3)"
       >
-        <div slot-scope="{suggestion}" style="display: flex; align-items: center;">
-          <img :src="`/countries/${suggestion.item.cca3.toLowerCase()}.svg`"
-               :style="{ display: 'flex', width: '25px', height: '25px', marginRight: '10px'}" width="50">
-          <div style="{ display: 'flex', color: 'navyblue'}">{{ suggestion.item.name.common }}</div>
+        <div
+            slot-scope="{suggestion}"
+            style="display: flex; align-items: center;"
+        >
+          <img
+              :src="`/countries/${suggestion.item.cca3.toLowerCase()}.svg`"
+              class="flex w-10 h-10 mr-4"
+          >
+          <div class="flex text-gray-800">{{ suggestion.item.name.common }}</div>
         </div>
       </vue-autosuggest>
 
-      <button class="button" @click="modalOpen = !modalOpen">Select multiple</button>
+      <vue-autosuggest
+          class="sm:mr-4 mb-2 sm:mb-0"
+          v-model="cityQuery"
+          :suggestions="filteredCities"
+          :get-suggestion-value="(city) => city.country"
+          :input-props="{id: 'autosuggest__input', class: 'input', placeholder: 'Enter a city'}"
+          @selected="addCity($event.item)"
+      >
+        <div slot-scope="{suggestion}">
+          <div class="flex text-gray-800">
+            {{ suggestion.item.name }}, {{ getCityCountry(suggestion.item) }}
+          </div>
+        </div>
+      </vue-autosuggest>
 
-      <div class="percent-visited">
-        {{ roundNumber((selectedCountries.length / 195) * 100) }}%
-      </div>
-    </div>
+      <div class="flex">
+        <button
+            class="bg-blue-900 mr-2 lg:mr-4 rounded-md text-white text-xs font-medium whitespace-nowrap px-4"
+            @click="modalOpen = !modalOpen"
+        >Select multiple
+        </button>
 
-    <div id="modal-select-countries" :class="{'open': modalOpen}">
-      <span class="modal-close" @click="modalOpen = false">&times;</span>
-
-      <div class="region" v-for="(countries, region) in filteredCountriesByRegion" :key="region">
-        <h2>{{ region }}</h2>
         <div
-            v-for="country in countries"
-            class="country"
-            :key="country.cca3"
-            :class="{'active': selectedCountries.includes(country.cca3)}"
-            @click="addCountry(country.cca3)"
+            class="mr-2 lg:mr-4 rounded-full text-white flex items-center content-center text-xs font-medium p-3 lg:px-4 whitespace-nowrap bg-indigo-700"
+            title="Number of visited countries"
         >
-          {{ country.name.common }}
+          <strong>{{ selectedCountries.length }}</strong>
+          <em>({{ roundNumber((selectedCountries.length / 195) * 100) }}%)</em>
+        </div>
+
+        <div
+            class="lg:mr-4 flex flex-1 items-center text-black cursor-pointer"
+        >
+          <input
+              class="input sm:w-24 w-full"
+              type="text"
+              :value="selectedCountriesAsEmojis"
+              onclick="this.select()"
+              readonly
+          >
         </div>
       </div>
     </div>
 
-    <Mapbox style="height: 100vh;" :zoom="2" :center="[-3.749220, 40.463669]">
+
+    <div
+        class="sm:grid-cols-3 fixed top-0 left-0 right-0 bottom-0 z-50 bg-white px-3 sm:px-5 text-left overflow-auto"
+        :class="{'grid': modalOpen, 'hidden': !modalOpen}"
+    >
+      <span
+          class="cursor-pointer fixed mr-2 mt-5 px-4 right-0 text-4xl top-0 z-10"
+          @click="modalOpen = false"
+      >&times;</span>
+
+      <div
+          v-for="(countries, region) in filteredCountriesByRegion"
+          :key="region"
+      >
+        <h2 class="font-medium text-2xl p-2 pt-6 sticky top-0 bg-white">{{ region }}</h2>
+        <div class="grid grid-cols-2">
+          <div
+              v-for="country in countries"
+              class="cursor-pointer relative text-sm sm:text-lg p-2"
+              :key="country.cca3"
+              :class="{'font-medium': selectedCountries.includes(country.cca3)}"
+              @click="addCountry(country.cca3)"
+          >
+            <span v-if="selectedCountries.includes(country.cca3)">
+              ✅
+            </span>
+            {{ country.name.common }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Mapbox
+        style="height: 100vh;"
+        :zoom="2"
+        :center="[-3.749220, 40.463669]"
+        @click="test"
+    >
       <MglGeojsonLayer
-          v-for="country in selectedCountries"
+          v-for="country in filteredSelectedCountries"
           :key="country"
           :sourceId="country"
           :source="countryGeoData[country]['source']"
           :layerId="country"
           :layer="countryGeoData[country]['layer']"
       />
+      <MglMarker v-for="city in selectedCities" :coordinates="[city.lng, city.lat]" :key="city.lng.toString() + city.lat.toString()"></MglMarker>
     </Mapbox>
   </div>
 </template>
 
 <script>
 import countries from './countries.json'
+import cities from './cities.json'
+import emojis from './emojis.js'
 import {VueAutosuggest} from 'vue-autosuggest';
 import Mapbox from "@/components/Mapbox";
-import {MglGeojsonLayer} from "vue-mapbox";
+import {MglGeojsonLayer, MglMarker} from "vue-mapbox";
+import {debounce} from "lodash";
 
 export default {
   name: 'Map',
-  components: {Mapbox, MglGeojsonLayer, VueAutosuggest},
+  components: {Mapbox, MglGeojsonLayer, VueAutosuggest, MglMarker},
   data() {
     return {
       query: '',
       map: null,
       selectedCountries: [],
       countryGeoData: {},
+      cities: cities,
+      selectedCities: [],
+      cityQuery: '',
+      debouncedCityQuery: '',
       modalOpen: false,
     }
   },
@@ -75,15 +146,47 @@ export default {
     const selectedCountries = JSON.parse(window.localStorage.getItem('selectedCountries')) || []
 
     this.initCountries(selectedCountries)
+
+    window.addEventListener('keyup', e => {
+      if (this.modalOpen && e.key === 'Escape') {
+        this.modalOpen = false
+      }
+    })
   },
 
   watch: {
     selectedCountries(val) {
       window.localStorage.setItem('selectedCountries', JSON.stringify(val))
-    }
+    },
+
+    cityQuery: debounce(function(val) {
+      this.debouncedCityQuery = val
+    }, 500)
   },
 
   computed: {
+    filteredCities() {
+      if (!this.debouncedCityQuery) return [{data: []}]
+      return [
+        {
+          data: this.cities.filter(city => {
+            return city.name.toLowerCase().includes(this.debouncedCityQuery.toLowerCase())
+          })
+        }
+      ];
+    },
+    filteredSelectedCountries() {
+      return this.selectedCountries.filter(c => {
+        return !!this.countryGeoData[c]
+      })
+    },
+    selectedCountriesAsEmojis() {
+      return this.selectedCountries.map(c => {
+        const country = this.countries.find(c2 => c2.cca3 === c)
+
+        return emojis[country.cca2]
+      }).join('')
+    },
     countries() {
       return countries.filter(c => c.independent)
     },
@@ -102,6 +205,21 @@ export default {
   },
 
   methods: {
+    getCityCountry(city) {
+      const country = this.countries.find(c => c.cca2 === city.country)
+
+      if (!country) return ''
+
+      return country.name.common
+    },
+    addCity(city) {
+      if (this.selectedCities.indexOf(city) === -1) {
+        this.selectedCities.push(city)
+      }
+    },
+    test(event) {
+      console.log(event)
+    },
     initCountries(countries) {
       countries.forEach(this.addCountry)
     },
@@ -122,6 +240,8 @@ export default {
         this.selectedCountries = this.selectedCountries.filter(c => c !== countryCode)
         return;
       }
+
+      this.selectedCountries.push(countryCode)
 
       this.query = ''
 
@@ -148,7 +268,7 @@ export default {
               }
             })
           }).then(() => {
-        this.selectedCountries.push(countryCode)
+        // this.selectedCountries.push(countryCode)
       })
     },
   },
@@ -232,19 +352,6 @@ export default {
   overflow: auto;
 }
 
-.country {
-  cursor: pointer;
-  padding: 3px;
-}
-
-.country:hover {
-  opacity: 0.7;
-}
-
-.country.active {
-  font-weight: bold;
-}
-
 .modal-close {
   position: absolute;
   top: 0;
@@ -254,11 +361,12 @@ export default {
   cursor: pointer;
 }
 
-#autosuggest__input {
+.input {
   border: 1px solid #ccc;
   border-radius: 6px;
   padding: 8px 12px;
   width: 100%;
+  outline: 0;
 }
 
 .map-input {
@@ -271,16 +379,28 @@ export default {
   margin: 20px;
 }
 
-.button {
-  background: #2caaa0;
+.select-multiple-button {
+  background: #343254;
+  font-weight: 600;
   border: 0;
   color: #fff;
-  border-radius: 4px;
+  border-radius: 6px;
   padding: 6px;
   font-size: 12px;
   width: 100%;
   flex: 0 0 120px;
   margin-left: 20px;
+  cursor: pointer;
+  outline: 0;
+}
+
+.select-multiple-button:hover {
+  background: #3c3a5f;
+}
+
+.select-multiple-button:focus,
+.select-multiple-button:active {
+  background: #46446d;
 }
 
 .percent-visited {
@@ -294,5 +414,6 @@ export default {
   font-weight: 600;
   padding: 9px;
   margin-left: 20px;
+  white-space: nowrap;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vue2TouchEvents from 'vue2-touch-events'
+import './assets/tailwind.css'
+
+Vue.use(Vue2TouchEvents)
 
 Vue.config.productionTip = false
 
